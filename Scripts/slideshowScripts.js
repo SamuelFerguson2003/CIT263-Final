ee8bdbc6c9e80c0d80c9d4f4030c884978c2584f
SamuelFerguson2003 <samueliferg@gmail.com>
2025-03-27 16:49:35 -0400

    Added scrolling banner for homepage!
    A few changes have been made to the gallery page, but those are nowhere close to final. This update focuses on making the homepage presentable and functional.

---
+++ Scripts/slideshowScripts.js
@@ -0,0 +1,17 @@
+/* This JavaScript was developed with the assistance of W3Schools */
+
+let slideNum = 0;
+showSlides();
+
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("slides");
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    slideNum++;
+    if (slideNum > slides.length) {slideNum = 1}
+    slides[slideNum-1].style.display = "block";
+    setTimeout(showSlides, 5000); // Change image every 2 seconds
+  }
