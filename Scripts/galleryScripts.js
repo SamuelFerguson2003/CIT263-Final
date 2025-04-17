e196f7d5afe15b8ca7bf871a9fe97007120d428b
SamuelFerguson2003 <samueliferg@gmail.com>
2025-04-17 10:20:51 -0400

    Delete galleryScripts.js
    Removed gallery modal stuff

--- Scripts/galleryScripts.js
+++
@@ -1,5 +0,0 @@
-/* This JavaScript was developed with the assistance of W3Schools 
-
-!!!! This file is going to contain the JavaScript for making the images open in a modla format when clicked.
-I'm just not sure how to do that yet :) */
-
